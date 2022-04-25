48e80874dbd335dbdd235b08e4e01fa1c857313c
Jessica Ortiz <70552512+JessicaOI@users.noreply.github.com>
2022-04-25 12:58:00 -0600

    Configuracion inicial

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+export default {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: {
+            directory: 'dist',
+        },
+        compress:true,
+        port: 9000,
+    },
+    module: {
+        rules: [{
+                test: /\.jsx?$/,
+                use: [{loader : 'babel-loader'}],
+            },
+            {
+                test: /\.css$/,
+                use: ['css-hot-loader',],
+            },
+            {
+                test: /\.(png|jpg|gif|svg|mp3)$/,
+                use: [{loader: 'file-loader'}]
+            },
+        ],
+    
+    },
+}
